cbbeb10e60ed26975098536c6865e42a1fbcdc00
nhutphi <phivnpc06272@gmail.com>
2024-11-08 18:33:44 +0700

    question

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import {ClassComponent} from "./pages/class/class.component";
+import { ClassComponent } from './pages/class/class.component';
+import { QuestionComponent } from './pages/question/question.component'; // Import QuestionComponent
 
-const routes: Routes = [{ path: 'class', component: ClassComponent }, ];
+const routes: Routes = [
+  { path: 'class', component: ClassComponent },
+  { path: 'question', component: QuestionComponent },  // Thêm route cho QuestionComponent
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,19 @@
-import {CUSTOM_ELEMENTS_SCHEMA, NgModule} from '@angular/core';
+import { CUSTOM_ELEMENTS_SCHEMA, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HeaderComponent } from './components/header/header.component';
 import { FooterComponent } from './components/footer/footer.component';
+import { HeaderComponent } from './components/header/header.component';
+import { ClassFormComponent } from './pages/class/class-form/class-form.component';
+import { ClassTableComponent } from './pages/class/class-table/class-table.component';
 import { ClassComponent } from './pages/class/class.component';
-import {ClassTableComponent} from "./pages/class/class-table/class-table.component";
-import {ClassFormComponent} from "./pages/class/class-form/class-form.component";
-import {HttpClientModule} from "@angular/common/http";
+
+// Import các component cho question
+import { QuestionFormComponent } from './pages/question/question-form/question-form.component';
+import { QuestionTableComponent } from './pages/question/question-table/question-table.component';
+import { QuestionComponent } from './pages/question/question.component';
 
 @NgModule({
   declarations: [
@@ -17,7 +22,10 @@ import {HttpClientModule} from "@angular/common/http";
     FooterComponent,
     ClassComponent,
     ClassTableComponent,
-    ClassFormComponent
+    ClassFormComponent,
+    QuestionComponent,               // Đảm bảo đã khai báo các component cho Question
+    QuestionTableComponent,
+    QuestionFormComponent
   ],
   imports: [
     BrowserModule,
@@ -26,6 +34,6 @@ import {HttpClientModule} from "@angular/common/http";
   ],
   providers: [],
   bootstrap: [AppComponent],
-  schemas: [CUSTOM_ELEMENTS_SCHEMA]
+  schemas: [CUSTOM_ELEMENTS_SCHEMA]  // Giữ lại nếu sử dụng Web Components
 })
 export class AppModule { }
